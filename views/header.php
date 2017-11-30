0c1aa46620b5d4a486558bb4f2b6a89c5bf0dc20
Kenny <kennygenly@gmail.com>
2017-11-30 16:37:35 +0100

    Add css for my view

--- views/header.php
+++ views/header.php
@@ -10,7 +10,7 @@
 		<!-- Place favicon.ico in the root directory -->
 		<link rel="stylesheet" href="css/bootstrap.css">
 		<link rel="stylesheet" href="css/normalize.css">
-		<link rel="stylesheet" href="css/main.css">
+		<link rel="stylesheet" href="../css/main.css">
 		
 		
 		<script src="js/vendor/modernizr-2.8.3.min.js"></script>
@@ -18,27 +18,13 @@
 	<body>
 	<header>
 		<figure>
-			<img style="width: 50%"; src="../img/LondonLib.jpg">
+			<img class="logo" src="../img/LondonLib.jpg">
 		</figure>
+		<h3 style="text-align: center; color: grey;">London Library Computer Application</h3>
 	</header>
+	
 
 
 
 
-	<footer></footer>	
-		<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
-		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
-		<script src="js/plugins.js"></script>
-		<script src="js/main.js"></script>
-		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-		<script>
-		(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-		function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-		e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-		e.src='https://www.google-analytics.com/analytics.js';
-		r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-		ga('create','UA-XXXXX-X','auto');ga('send','pageview');
-		</script>
-		<script src="js/bootstrap.js"></script>
-	</body>
-</html>
+<!--  -->
